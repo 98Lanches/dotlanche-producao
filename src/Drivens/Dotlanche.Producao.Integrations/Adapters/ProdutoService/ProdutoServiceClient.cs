227dcafe62596247fc89689842ac3458e906b71e
Andre Torres <atcorrea17@gmail.com>
2024-11-07 21:36:58 -0300

    feat: Add produto service integration

---
+++ src/Drivens/Dotlanche.Producao.Integrations/Adapters/ProdutoService/ProdutoServiceClient.cs
@@ -0,0 +1,35 @@
+﻿using Dotlanche.Producao.Domain.Entities;
+using Dotlanche.Producao.Domain.Ports;
+using Dotlanche.Producao.Integrations.Adapters.ProdutoService.Responses;
+using System.Text.Json;
+
+namespace Dotlanche.Producao.Integrations.Adapters.ProdutoService
+{
+    internal class ProdutoServiceClient : IProdutoServiceClient
+    {
+        private readonly HttpClient client;
+        private readonly JsonSerializerOptions defaultJsonOptions = new() { PropertyNameCaseInsensitive = true };
+
+        public ProdutoServiceClient(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task<IEnumerable<Produto>> GetByIds(IEnumerable<Guid> ids, CancellationToken cancellationToken = default)
+        {
+            // TODO: atualizar quando tiver o serviço de produtos real
+            const string getProductsByIdsUrl = "/produtos";
+
+            var response = await client.GetAsync(getProductsByIdsUrl, cancellationToken);
+
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync(cancellationToken);
+            var produtoResponse = JsonSerializer.Deserialize<IEnumerable<GetProdutosByIdsResponse>>(content, defaultJsonOptions) ?? [];
+
+            var produtos = produtoResponse.Select(x => new Produto(x.Id, x.Name, x.Categoria, x.Price));
+
+            return produtos;
+        }
+    }
+}
